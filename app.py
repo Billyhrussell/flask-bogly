509d23f401e56b364c124d0f90fa45ae78b7229e
Aishwarya Majumdar <aishu.maj1996@gmail.com>
2022-08-03 14:52:48 -0700

    added first four routes and sample data

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Blogly application."""
 
-from flask import Flask
+from flask import Flask, render_template, redirect, request
 from models import db, connect_db, User
 
 app = Flask(__name__)
@@ -14,5 +14,25 @@ debug = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
+
+@app.get("/")
+def homepage():
+    """TODO: """
+
+    return redirect("/users")
+
+@app.get("/users")
+def user_list():
+    """TODO: """
+
+    users = User.query.all()
+    return render_template("userlisting.html", users = users)
+
+@app.get("/users/new")
+def add_new_user_form():
+    """TODO: """
+
+    return render_template("newuser.html")
+
 
 
