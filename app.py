7f31ad67d4efb514b7bec5bab2ec628ea3e11330
Billyhrussell <billy.hrussell@gmail.com>
2022-08-03 16:37:12 -0700

    part one fin. no tests yet

--- app.py
+++ app.py
@@ -34,12 +34,14 @@ def add_new_user_form():
 
     return render_template("newuser.html")
 
-app.post("/users/new")
+@app.post("/users/new")
 def add_new_user():
     #process add form
     first_name = request.form['first-name']
     last_name = request.form['last-name']
     img_url = request.form['image-url']
+
+    img_url = img_url if img_url else None
 
     new_user = User(first_name = first_name, last_name = last_name, img_url = img_url)
 
@@ -50,7 +52,40 @@ def add_new_user():
     #go back to users
     return redirect("/users")
 
+@app.get("/users/<user_id>")
+def display_user(user_id):
+    #show information about user
+    user_information = User.query.get(user_id)
 
+    return render_template("userdetail.html", user = user_information)
 
+@app.get("/users/<user_id>/edit")
+def edit_user(user_id):
+    #show the edit page for a user
+    user_information = User.query.get(user_id)
+    return render_template("edit.html", user = user_information)
 
+@app.post("/users/<user_id>/edit")
+def update_user(user_id):
+    #process edit form
+    user = User.query.get(user_id)
 
+    user.first_name = request.form['first-name']
+    user.last_name = request.form['last-name']
+    user.img_url = request.form['image-url']
+    #user.img_url = img_url if img_url else None
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect("/users")
+
+@app.post("/users/<user_id>/delete")
+def delete_user(user_id):
+    #delete a users information
+    user = User.query.get(user_id)
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return redirect("/users")
