8ddf28bd99376b3d34dfc6fed442ff5b068c8fba
Aishwarya Majumdar <aishu.maj1996@gmail.com>
2022-08-04 10:08:27 -0700

    seed data urls

--- seed.py
+++ seed.py
@@ -7,11 +7,11 @@ db.drop_all()
 db.create_all()
 
 aishwarya = User(first_name = 'Aishwarya', last_name = 'Majumdar',
-                img_url = 'https://stanfordopticians.co.uk/wp-content/uploads/2016/04/default-profile.png')
+                img_url = 'https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-jumbo.jpg?quality=75&auto=webp')
 billy = User(first_name = 'Billy', last_name='Russell',
-                img_url ='https://stanfordopticians.co.uk/wp-content/uploads/2016/04/default-profile.png')
+                img_url ='https://i.insider.com/5484d9d1eab8ea3017b17e29?width=600&format=jpeg&auto=webp')
 steve = User(first_name = 'Steve', last_name = 'Martin',
-                img_url = 'https://stanfordopticians.co.uk/wp-content/uploads/2016/04/default-profile.png')
+                img_url = 'https://prdaficalmjediwestussa.blob.core.windows.net/images/2019/07/0003_COLOR_2015_LAA_SteveMartin_-557716299_KW_8679_AE182BAE8D02C2DDB953723A94E7A3FF.jpg')
 
 
 db.session.add(aishwarya)
